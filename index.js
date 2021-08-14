eec2647bbd83de2a1b7044570a26c7f5af6df93e
ukunV <showklee@naver.com>
2021-08-14 11:45:19 +0900

    initial commit

---
+++ index.js
@@ -0,0 +1,15 @@
+const express = require("./config/express");
+const { logger } = require("./config/winston");
+
+let port;
+
+if (process.env.NODE_ENV === "development") {
+  port = 3001;
+} else if (process.env.NODE_ENV === "production") {
+  port = 3000;
+} else {
+  port = 9000;
+}
+
+express().listen(port);
+logger.info(`${process.env.NODE_ENV} - API Server Start At Port ${port}`);
