71e74d9b8b6be90aafe625df7c5ef27caa12c961
ukunV <showklee@naver.com>
2021-08-18 01:29:25 +0900

    add: getQueryAddressInfo

--- controllers/kakao_ctrl.js
+++ controllers/kakao_ctrl.js
@@ -18,6 +18,22 @@ const getAddressInfo = async (address) => {
   return { lat, lng };
 };
 
+const getQueryAddressInfo = async (address) => {
+  const response = await axios({
+    method: "GET",
+    url: `https://dapi.kakao.com/v2/local/search/address.json?analyze_type=similar&query=${address}`,
+    headers: {
+      Authorization: `KakaoAK ${sercetKey}`,
+    },
+  });
+
+  const lat = response.data.documents[0].road_address.x;
+  const lng = response.data.documents[0].road_address.y;
+
+  return { lat, lng };
+};
+
 module.exports = {
   getAddressInfo,
+  getQueryAddressInfo,
 };
