f108f932b9d286563a8d687785998c4d7c892d31
ukunV <showklee@naver.com>
2021-08-22 17:49:42 +0900

    remove: getQueryAddressInfo

--- controllers/kakao_ctrl.js
+++ controllers/kakao_ctrl.js
@@ -18,22 +18,6 @@ const getAddressInfo = async (address) => {
   return { lat, lng };
 };
 
-const getQueryAddressInfo = async (address) => {
-  const response = await axios({
-    method: "GET",
-    url: `https://dapi.kakao.com/v2/local/search/address.json?analyze_type=similar&query=${address}`,
-    headers: {
-      Authorization: `KakaoAK ${sercetKey}`,
-    },
-  });
-
-  const lat = response.data.documents[0].address.x;
-  const lng = response.data.documents[0].address.y;
-
-  return { lat, lng };
-};
-
 module.exports = {
   getAddressInfo,
-  getQueryAddressInfo,
 };
